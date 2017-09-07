244718369e51cf729d7c3c68d8b8565c724f71a2
wren <wren@lawn-128-61-120-14.lawn.gatech.edu>
2017-09-07 13:42:04 -0400

    Changes to person4 file

--- M2/src/main/java/edu/gatech/oad/antlab/person/Person4.java
+++ M2/src/main/java/edu/gatech/oad/antlab/person/Person4.java
@@ -3,8 +3,8 @@ package edu.gatech.oad.antlab.person;
 /**
  *  A simple class for person 4
  *  returns their name and a
- *  modified string 
- *  
+ *  modified string
+ *
  *  @author Bob
  *  @version 1.1
  */
@@ -21,7 +21,7 @@ public class Person4 {
   }
     /**
      * This method should return a string
-     * where each character is 1 greater 
+     * where each character is 1 greater
      * than its previous value.  So
      * given "abc123" it should return
      * "bcd234".
@@ -31,15 +31,27 @@ public class Person4 {
      */
     private String calc(String input) {
       //Person 4 put your implementation here
-      return null;
-    }
-    
+      char[] in = input.toCharArray();
+     int begin = 0;
+     int end = in.length - 1;
+     char temp;
+     while (end>begin) {
+         temp = in[begin];
+         in[begin]=in[end];
+         in[end] = temp;
+         end--;
+         begin++;
+   }
+   return new String(in);
+}
+
+
     /**
      * Return a string rep of this object
      * that varies with an input string
      *
      * @param input the varying string
-     * @return the string representing the 
+     * @return the string representing the
      *         object
      */
     public String toString(String input) {
@@ -47,4 +59,3 @@ public class Person4 {
     }
 
 }
-
