1aedb482f9e629d2dca9ff37ca5d45c8c6e6f792
Jay Zhang <jayzhang@lawn-128-61-62-151.lawn.gatech.edu>
2017-09-07 13:05:34 -0400

    updated person1's method

--- M2/src/main/java/edu/gatech/oad/antlab/person/Person1.java
+++ M2/src/main/java/edu/gatech/oad/antlab/person/Person1.java
@@ -3,8 +3,8 @@ package edu.gatech.oad.antlab.person;
 /**
  *  A simple class for person 1
  *  returns their name and a
- *  modified string 
- *  
+ *  modified string
+ *
  *  @author Bob
  *  @version 1.1
  */
@@ -31,19 +31,27 @@ public class Person1 {
 	 */
 	private String calc(String input) {
 	  //Person 1 put your implementation here
-	  return null;
+	  String output = "";
+	  for (int i = 0; i < input.length(); i++) {
+          output += input.charAt((i + 2) % input.length());
+      }
+      return output;
 	}
-	
+
 	/**
 	 * Return a string rep of this object
 	 * that varies with an input string
 	 *
 	 * @param input the varying string
-	 * @return the string representing the 
+	 * @return the string representing the
 	 *         object
 	 */
 	public String toString(String input) {
 	  return name + calc(input);
 	}
 
+    public static void main(String[] args) {
+        Person1 p = new Person1("j");
+        System.out.println(p.toString("gtg123b"));
+    }
 }
