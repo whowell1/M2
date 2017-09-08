7e52d962b0b4989a76e3cb5d8dc4c392603d9ddc
dbhavsar202 <dbhavsar202@gmail.com>
2017-09-08 18:13:21 -0400

    Devon updated his file

--- M2/src/main/java/edu/gatech/oad/antlab/person/Person2.java
+++ M2/src/main/java/edu/gatech/oad/antlab/person/Person2.java
@@ -1,17 +1,20 @@
 package edu.gatech.oad.antlab.person;
+import java.util.*;
 
 /**
  *  A simple class for person 2
  *  returns their name and a
- *  modified string 
+ *  modified string
  *
- * @author Bob
+ * @author Devon Bhavsar
  * @version 1.1
  */
 public class Person2 {
+
     /** Holds the persons real name */
     private String name;
-	 	/**
+
+    /**
 	 * The constructor, takes in the persons
 	 * name
 	 * @param pname the person's real name
@@ -19,6 +22,7 @@ public class Person2 {
 	 public Person2(String pname) {
 	   name = pname;
 	 }
+
 	/**
 	 * This method should take the string
 	 * input and return its characters in
@@ -31,14 +35,22 @@ public class Person2 {
 	 */
 	private String calc(String input) {
 	  //Person 2 put your implementation here
-	  return null;
+		String randomString = "";
+		while (input.length() != 0) {
+			int randomIndex = (int) (Math.random() * input.length());
+			char c = input.charAt(randomIndex);
+			input = input.substring(0, randomIndex) + input.substring(randomIndex + 1);
+			randomString += c;
+		}
+	  	return randomString;
 	}
+
 	/**
 	 * Return a string rep of this object
 	 * that varies with an input string
 	 *
 	 * @param input the varying string
-	 * @return the string representing the 
+	 * @return the string representing the
 	 *         object
 	 */
 	public String toString(String input) {
