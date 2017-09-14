29a2da6793a2c9d59dc082d72ebf2ae697282775
Jay Zhang <jayzhang@lawn-128-61-55-189.lawn.gatech.edu>
2017-09-14 14:02:53 -0400

    changed name

--- M2/src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
+++ M2/src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
@@ -73,7 +73,7 @@ import edu.gatech.oad.antlab.pkg2.*;
                   toPrint += p4.toString("whowell35");
                   //Person5 replace P4 with your name
                   //and gburdell5 with your gt id
-                  Person5 p5 = new Person5("P5");
+                  Person5 p5 = new Person5(" Thomas Olsen ");
                   toPrint += p5.toString("tolson9");
 
         System.out.println(toPrint);
